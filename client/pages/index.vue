66a0ee99d916eba5d6337da0abf2f8829addf2aa
Aldous <estacioaldous@gmail.com>
2024-11-10 20:25:30 +0800

    refactor(index.vue): selection removed

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,120 +1,86 @@
 <template>
-	<v-container>
-		<h2>
-			<v-icon icon="mdi-vuetify" />
-			Starter Template
-		</h2>
+	<v-container class="border">
+		<h1>Welcome to Apollo System Exam</h1>
+		<h4>Here I am going todo some Task utilizing the techs below:</h4>
 		<h5>Nuxt 3 / Vuetify / Graphql / Pinia</h5>
-		<h3 class="my-5">
-			Example Pinia
-			<v-chip color="blue">useCounter</v-chip>
-		</h3>
-		<v-card class="mx-auto my-12" max-width="374">
-			<v-card-title class="text-blue">Pinia useCounter()</v-card-title>
-			<v-card-item>
-				<v-card-text>
-					<v-chip>count:</v-chip>
-					{{ store.count }}
-				</v-card-text>
-				<v-card-text>
-					<v-chip>doubleCount:</v-chip>
-					{{ store.doubleCount }}
-				</v-card-text>
-			</v-card-item>
-
-			<v-card-actions><v-btn color="blue" @click="store.increment()">Increment</v-btn></v-card-actions>
-		</v-card>
-
-		<h3 class="my-5">
-			Example Vuetify
-			<v-chip color="blue">Card</v-chip>
-		</h3>
-		<v-card class="mx-auto my-12" max-width="374">
-			<template #progress>
-				<v-progress-linear color="deep-purple" height="10" indeterminate />
-			</template>
-
-			<v-img height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png" />
-
-			<v-card-title>Cafe Badilico</v-card-title>
-
-			<v-card-text>
-				<v-row align="center" class="mx-0">
-					<v-rating :value="4.5" color="amber" dense half-increments readonly size="14" />
-
-					<div class="grey--text ms-4">4.5 (413)</div>
+		<v-container class="border mt-5 bg-grey-darken-4 rounded-lg">
+			<h3>Display Launches using Pages</h3>
+			<!-- <pre>{{ launches }}</pre> -->
+			<v-container>
+				<v-row>
+					<!-- Loop through each launch in the launches array -->
+					<v-col v-for="launch in launches" :key="launch.id" cols="12" md="4">
+						<v-card class="mb-4 h-100" link>
+							<v-card-title>Mission: {{ launch.mission_name }}</v-card-title>
+							<v-card-text>
+								<p>Rocket: {{ launch.rocket.rocket_name }}</p>
+								<p>Site: {{ launch.launch_site?.site_name || 'Unknown' }}</p>
+								<v-card-actions>
+									<p>Click arrow for details</p>
+									<v-spacer />
+									<v-btn
+										:icon="
+											expandedLaunches.has(launch.id)
+												? 'mdi-chevron-up'
+												: 'mdi-chevron-down'
+										"
+										@click="toggleDetails(launch.id)"
+									/>
+								</v-card-actions>
+								<v-expand-transition>
+									<div v-show="expandedLaunches.has(launch.id)">
+										<p>{{ launch?.details || 'No Details Provided' }}</p>
+									</div>
+								</v-expand-transition>
+							</v-card-text>
+							<v-card-subtitle>{{ launch.launch_date_local }}</v-card-subtitle>
+						</v-card>
+					</v-col>
 				</v-row>
-
-				<div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
-
-				<div>
-					Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio
-					seating.
-				</div>
-			</v-card-text>
-
-			<v-divider class="mx-4" />
-
-			<v-card-title>Tonight's availability</v-card-title>
-
-			<v-card-text>
-				<v-chip-group v-model="selection" active-class="deep-purple accent-4 white--text" column>
-					<v-chip>5:30PM</v-chip>
-
-					<v-chip>7:30PM</v-chip>
-
-					<v-chip>8:00PM</v-chip>
-
-					<v-chip>9:00PM</v-chip>
-				</v-chip-group>
-			</v-card-text>
-
-			<v-card-actions>
-				<v-btn color="deep-purple lighten-2">Reserve</v-btn>
-			</v-card-actions>
-		</v-card>
-		<h3 class="my-5">
-			Example Vuetify
-			<v-chip color="blue">SimpleTable</v-chip>
-			<v-chip color="orange">Data from spaceX graphql</v-chip>
-		</h3>
-		<p>There are {{ ships?.length || 0 }} ships.</p>
-		<v-table>
-			<thead>
-				<tr>
-					<th class="text-left">Name</th>
-					<th class="text-left">Active</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr v-for="ship in ships" :key="ship.name">
-					<td>{{ ship.name }}</td>
-					<td>
-						<v-chip :color="ship.active ? 'green' : 'red'">{{ ship.active }}</v-chip>
-					</td>
-				</tr>
-			</tbody>
-		</v-table>
+			</v-container>
+		</v-container>
 	</v-container>
 </template>
 <script lang="ts" setup>
-const store = useCounter()
-const selection = ref(0)
 const query = gql`
-	query getShips {
-		ships {
+	query getLaunches {
+		launches {
+			mission_name
 			id
-			name
-			active
+			rocket {
+				rocket_name
+			}
+			details
+			launch_date_local
+			launch_site {
+				site_name
+			}
 		}
 	}
 `
 const { data } = useAsyncQuery<{
-	ships: {
-		id: String
-		name: String
-		active: Boolean
+	launches: {
+		mission: String
+		id: number
+		rocket: {
+			rocket_name: String
+		}
+		details: String
+		launch_date_local: String
+		launch_site: {
+			site_name: String
+		}
 	}[]
 }>(query)
-const ships = computed(() => data.value?.ships ?? [])
+const launches = computed(() => data.value?.launches ?? [])
+const expandedLaunches = ref(new Set<number>())
+
+// Toggle the visibility of details for a specific launch
+function toggleDetails(id: number) {
+	if (expandedLaunches.value.has(id)) {
+		expandedLaunches.value.delete(id)
+	} else {
+		expandedLaunches.value.add(id)
+	}
+}
 </script>
---
+++ client/pages/launches/index.vue
@@ -0,0 +1,5 @@
+<script></script>
+
+<template>
+	<div>Hellow pow</div>
+</template>
