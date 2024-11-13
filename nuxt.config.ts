ecb934400f8ef8f9eb9f5daa0e4653380338b588
Aldous <estacioaldous@gmail.com>
2024-11-14 03:28:05 +0800

    fix: fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,39 +2,18 @@ import vuetify from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-	head: {
-		link: [{ rel: 'shortcut icon', type: 'image/x-icon', href: './favicon.ico' }],
-	},
 	srcDir: 'client/',
-	build: {
-		target: 'static',
-		css: ['vuetify/styles/global.css'],
-	},
-	imports: {
-		dirs: ['./stores', './composables', './components'],
-	},
+	build: { transpile: ['vuetify'] },
+	imports: { dirs: ['./stores', './composables', './components'] },
 	apollo: {
 		autoImports: true,
 		proxyCookies: true,
 		clients: {
-			default: {
-				httpEndpoint: 'https://spacex-production.up.railway.app/',
-			},
+			default: { httpEndpoint: 'https://spacex-production.up.railway.app/' },
 		},
 	},
 	vite: {
 		plugins: [vuetify()],
-		alias: {
-			'@': './client',
-		},
 	},
-	modules: [
-		'@nuxtjs/apollo',
-		[
-			'@pinia/nuxt',
-			{
-				autoImports: ['defineStore', 'acceptHMRUpdate'],
-			},
-		],
-	],
+	modules: ['@nuxtjs/apollo', ['@pinia/nuxt', { autoImports: ['defineStore', 'acceptHMRUpdate'] }]],
 })
